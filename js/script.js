3a45c4dc1901d14f8e1c4449a3d4dbd31fb6ff4c
Łukasz <lukasz.niezbecki@gmail.com>
2022-11-17 13:15:37 +0100

    added onclick to contacts and social media  icons, added hover on contact icons, added function showing actual  fotter year in JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//znikanie nawigacji po kliknięciu w link
+
 document.addEventListener("DOMContentLoaded", function () {
 	const allNavLinks = document.querySelectorAll(".nav-link");
 	const navList = document.querySelector(".navbar-collapse");
@@ -6,3 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
 		link.addEventListener("click", () => navList.classList.remove("show"))
 	);
 });
+
+//aktualny rok w stopce
+
+const footerYear = document.querySelector(".footer-year");
+
+const CurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+CurrentYear();
